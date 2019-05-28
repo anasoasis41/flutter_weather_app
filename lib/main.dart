c4c6bf653236b45df1f4ddc6da1097c97ec3f59a
Anas Riahi <anas.riahi@smile-maroc.com>
2019-05-28 15:29:26 +0000

    Add Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -27,19 +27,79 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  List<String> cities = ["Marrakech", "Rabat", "Casablanca"];
+  String selectedCity;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+      drawer: new Drawer(
+        child: new Container(
+          child: new ListView.builder(
+            itemCount: cities.length + 2,
+              itemBuilder: (context, i) {
+              if (i == 0) {
+                return DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.lightBlue),
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      textWithStyle("Cities", fontSize: 22.0),
+                      new RaisedButton(
+                        color: Colors.white,
+                          elevation: 8.0,
+                          child: textWithStyle("Add City", color: Colors.blue),
+                          onPressed: () {
 
-          ],
+                          }),
+                    ],
+                  ),
+                );
+              } else if (i == 1) {
+                return new ListTile(
+                  title: textWithStyle("My current city"),
+                  onTap: () {
+                    setState(() {
+                      selectedCity = null;
+                      Navigator.pop(context);
+                    });
+                  },
+                );
+              } else {
+                String city = cities[i - 2];
+                return new ListTile(
+                  title: textWithStyle(city),
+                  onTap: () {
+                    setState(() {
+                      selectedCity = city;
+                      Navigator.pop(context);
+                    });
+                  },
+                );
+              }
+              }),
+          color: Colors.blue,
         ),
+      ),
+      body: Center(
+        child: new Text((selectedCity == null)? "Current city": selectedCity),
+      ),
+    );
+  }
+
+
+  Text textWithStyle(String data, {color: Colors.white, fontSize: 18.0, fontStyle: FontStyle.italic, textAlign: TextAlign.center}) {
+    return new Text(
+      data,
+      textAlign: textAlign,
+      style: new TextStyle(
+        color: color,
+        fontSize: fontSize,
+        fontStyle: fontStyle
       ),
     );
   }
